975d94d91c675ae4b68f8daf52d1ab2738bf9139
joshuagame <joshuagame@gmail.com>
2019-06-21 16:52:37 +0200

    Serve() now to serve custom status message

--- pkg/health/health.go
+++ pkg/health/health.go
@@ -16,20 +16,26 @@ type status struct {
 	Status string `json:"status"`
 }
 
-// ServeDefault serve the default health {"status": "UP"} endpoint
-func ServeDefault() {
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		status := status{"UP"}
-		jsonStatus, err := json.Marshal(status)
+func healthHandler(s interface{}) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		jsonStatus, err := json.Marshal(s)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(200)
 		w.Write(jsonStatus)
 	})
+}
 
-	log.Fatal(http.ListenAndServe(":3010", nil))
+// ServeDefault serve the default health {"status": "UP"} endpoint
+func ServeDefault(addr string) {
+	Serve(addr, status{"UP"})
+}
+
+// Serve starts the health endpoint listening on addr and serving the s status
+func Serve(addr string, s interface{}) {
+	http.Handle("/health", healthHandler(s))
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
